5f3b7000c0fc6c28086e72b3648a0436c59a6d5b
achomadu onyinye <achomaduonyinye@gmail.com>
2022-09-08 16:00:14 +0100

    make it better

--- app.js
+++ app.js
@@ -11,20 +11,21 @@ const weatherData = require('./utils/weatherData');
 const port = process.env.PORT || 5000
 
 // Public Static file path Setup
-const publicDirectoryPath = path.join(__dirname, '../public')
+const publicDirectoryPath = path.join(__dirname, '/public')
 
 // Setting up the view template Route
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const viewsPath = path.join(__dirname, './templates/views')
+const partialsPath = path.join(__dirname, './templates/partials')
 
 
 //Setup the view engine
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
 hbs.registerPartials(partialsPath)
-
 //using the publicDirectorypath items
 app.use(express.static(publicDirectoryPath))
+
+
 
 app.get('/', (req, res)=>{
     res.render('index',{
